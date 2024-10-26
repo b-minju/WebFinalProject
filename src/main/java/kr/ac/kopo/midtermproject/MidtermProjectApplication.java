f4120702417c0054bf462e3d40955c0cda1a70a4
KIMPROSMINJU <b.m1nj00371@gmail.com>
2024-10-26 22:42:50 +0900

    home, notice, signup page and login modal done

---
+++ src/main/java/kr/ac/kopo/midtermproject/MidtermProjectApplication.java
@@ -0,0 +1,13 @@
+package kr.ac.kopo.midtermproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MidtermProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MidtermProjectApplication.class, args);
+    }
+
+}
