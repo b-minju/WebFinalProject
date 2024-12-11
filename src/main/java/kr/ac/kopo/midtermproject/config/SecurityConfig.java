6449494266b8764d61f04ab3a85ea6f2e47357eb
KIMPROSMINJU <b.m1nj00371@gmail.com>
2024-12-11 15:53:36 +0900

    security config done

---
+++ src/main/java/kr/ac/kopo/midtermproject/config/SecurityConfig.java
@@ -0,0 +1,39 @@
+package kr.ac.kopo.midtermproject.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+        // CSRF 보안 비활성화
+        httpSecurity.csrf(AbstractHttpConfigurer::disable);
+        httpSecurity.cors(Customizer.withDefaults());
+
+        //FormLogin, BasicHttp 비활성화
+        httpSecurity.formLogin(AbstractHttpConfigurer::disable);
+        httpSecurity.httpBasic(AbstractHttpConfigurer::disable);
+
+        httpSecurity.authorizeHttpRequests(authorize -> authorize
+                .requestMatchers("/notice/**").hasAnyRole("USER", "ADMIN", "MANAGER") // "/notice/"로 시작하는 모든 요청에 대해 USER, ADMIN, MANAGER 역할 허용
+                .anyRequest().permitAll() // 그 외의 요청은 인증 없이 허용
+        );
+
+        return httpSecurity.build();
+    }
+}
--- src/main/java/kr/ac/kopo/midtermproject/service/UserEntityServiceImpl.java
+++ src/main/java/kr/ac/kopo/midtermproject/service/UserEntityServiceImpl.java
@@ -5,6 +5,7 @@ import kr.ac.kopo.midtermproject.entity.UserEntity;
 import kr.ac.kopo.midtermproject.repository.UserEntityRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -12,6 +13,8 @@ import org.springframework.stereotype.Service;
 public class UserEntityServiceImpl implements UserEntityService{
     @Autowired
     private UserEntityRepository userEntityRepository;
+    @Autowired
+    private BCryptPasswordEncoder passwordEncoder;
 
 
     @Override
